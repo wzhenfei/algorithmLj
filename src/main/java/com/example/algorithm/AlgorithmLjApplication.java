d266e120a7c26d0f8c2a74b9f1814dfe278e0a56
wzf <2661206226@qq.com>
2022-08-08 20:43:32 +0800

    add

---
+++ src/main/java/com/example/algorithm/AlgorithmLjApplication.java
@@ -0,0 +1,13 @@
+package com.example.algorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmLjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmLjApplication.class, args);
+    }
+
+}
